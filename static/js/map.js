2b5b2232112b6dc7833ccae0978344d8e05888b4
Anna Propas <apropas@gmail.com>
2017-09-24 12:18:17 -0700

    adds animation

--- static/js/map.js
+++ static/js/map.js
@@ -49,12 +49,12 @@
                         .classed("selected", true)
                         .attr("data-toggle", "true");
                 }
-                // uncomment for zoom on click. needs work. does not toggle
-                // var centroid = path.centroid(d);
-                // x = centroid[0];
-                // y = centroid[1];
-                // d3.select("g").transition()
-                //     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + 4 + ")translate(" + -x + "," + -y + ")")
+                //uncomment for zoom on click. needs work. does not toggle
+                var centroid = path.centroid(d);
+                x = centroid[0];
+                y = centroid[1];
+                d3.select("g").transition()
+                    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + 4 + ")translate(" + -x + "," + -y + ")")
             })
             // show tooltip when country is moused-over
             .on("mouseover", function (d) {
@@ -68,34 +68,6 @@
             .on("mouseout", function (d) {
                 tooltip.attr("class", "inactive");
             });
-        // add circles to svg
-        // var coord1 = [-122.490402, 37.786453];
-        // var coord2 = [-99.912437, 16.848824];
-        // var locales = { "San Francisco": { lat: -122.4194, lon: 37.7749, avg: 25, num: 89 }, "Acapulco": { lat: -99.912437, lon: 16.848824, avg: 12, num: 120 }, "Mozambique": { lat: -25.953724, lon: 32.588711, avg: 8, num: 12 }, "Prague": { lat: -50.0755, lon: -14.4378, avg: 34, num: 78 } }
-        // test data for setting up markers
-
-        // timer for marker pulse
-        // setInterval(function(){
-        //     svg.selectAll("circle")
-        //         .transition()
-        //         .duration(1000)
-        //         .attr('r', function (d) {
-        //             // console.log(d)
-        //             return d[2]
-        //         })
-        //         .on("end", function (d) {
-        //             svg.selectAll("circle")
-        //                 .transition()
-        //                 .duration(1000)
-        //                 .attr('r', function (d) {
-        //                     // console.log(d)
-        //                     return 2
-        //                 })
-        //         })
-        // }, 1000)
-        // d3.select("svg").on("mousedown.log", function () {
-        //     console.log(projection.invert(d3.mouse(this)));
-        // });
     }
     // materialize modal trigger
     $('.modal').modal({dismissible: true});
@@ -129,7 +101,27 @@
                     coords.push(d[1], d[0])
 
                 })
-            $('.preloader-wrapper').removeClass("active").addClass("inactive")
+            $('.preloader-wrapper').removeClass("active").addClass("inactive");
+                    // timer for marker pulse
+            setInterval(function(){
+                svg.selectAll("circle")
+                    .transition()
+                    .duration(500)
+                    .attr('r', function (d) {
+                        return parseInt(d[2])/7
+                    })
+                    .on("end", function (d) {
+                        svg.selectAll("circle")
+                            .transition()
+                            .duration(500)
+                            .attr('r', function (d) {
+                                return 2
+                            })
+                    })
+            }, 1000)
+            // d3.select("svg").on("mousedown.log", function () {
+            //     console.log(projection.invert(d3.mouse(this)));
+            // });
         });
     // });
 
